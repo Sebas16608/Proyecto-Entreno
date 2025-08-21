6e332540eb8e21f4be431b90513784f4ca179141
Sebas16608 <Asebasrr444@gmail.com>
2025-08-20 18:27:39 -0600

    Instalando apps

--- backend/entrenamiento/settings.py
+++ backend/entrenamiento/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rutina.apps.RutinaConfig',
+    'user.apps.UserConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
