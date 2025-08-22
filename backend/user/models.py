b3b69ca344119438738fe2ad1a5f46aca3fe0548
Sebas16608 <Asebasrr444@gmail.com>
2025-08-21 18:18:33 -0600

    Creando una carpeta para crear la API con flask

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
+class User(AbstractBaseUser):
+    full_name = models.CharField(max_length=100)
+
+class Profile(models.Model):
+    photo = models.ImageField(upload_to='')
